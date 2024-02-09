75fe27c34c89ddbac4b9ac52913afe4577da4ae4
Aleksei Menshikh <amenshih@lanta-net.ru>
2024-02-09 10:47:01 +0300

    Fix speakerphone during video calls; remove unnecessary permissions in manifest

--- presentation/src/main/java/com/sesameware/smartyard_oem/ui/call/AndroidAudioManager.kt
+++ presentation/src/main/java/com/sesameware/smartyard_oem/ui/call/AndroidAudioManager.kt
@@ -9,27 +9,13 @@ import android.os.Vibrator
  * Created on 23.07.2020.
  */
 class AndroidAudioManager(var context: Context) {
-
     private var mAudioManager: AudioManager? = null
         private set
     var vibrator: Vibrator? = null
         private set
 
     init {
-        mAudioManager =
-            context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        mAudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-    }
-
-    fun routeAudioToEarPiece() {
-        routeAudioToSpeakerHelper(false)
-    }
-
-    fun routeAudioToSpeaker() {
-        routeAudioToSpeakerHelper(true)
-    }
-
-    private fun routeAudioToSpeakerHelper(speakerOn: Boolean) {
-        mAudioManager?.isSpeakerphoneOn = speakerOn
     }
 }
