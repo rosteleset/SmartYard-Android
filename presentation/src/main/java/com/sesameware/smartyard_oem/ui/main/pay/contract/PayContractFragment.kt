70a53d04f0a9442d45c3ccf2cf82f780c0410c18
izhukov <izhukov@lanta-net.ru>
2024-03-04 10:45:30 +0300

    Fixed TextViews with potentially long text. Fixed the indentation of the main content from the top background image on several layouts.

--- presentation/src/main/java/com/sesameware/smartyard_oem/ui/main/pay/contract/PayContractFragment.kt
+++ presentation/src/main/java/com/sesameware/smartyard_oem/ui/main/pay/contract/PayContractFragment.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
