08a2a20e2a7d79c923aa085e274d3b707c79a1f4
Aleksei Menshikh <amenshih@lanta-net.ru>
2024-08-02 12:30:39 +0300

    Add system info and language headers to http requests

--- data/src/main/java/com/sesameware/data/interceptors/SessionInterceptor.kt
+++ data/src/main/java/com/sesameware/data/interceptors/SessionInterceptor.kt
@@ -1,10 +1,12 @@
 package com.sesameware.data.interceptors
 
+import android.os.Build
 import com.sesameware.data.BuildConfig
 import com.sesameware.data.DataModule
 import okhttp3.Interceptor
 import okhttp3.Response
 import com.sesameware.data.prefs.PreferenceStorage
+import java.util.Locale
 
 /**
  * @author Artem Budarin
@@ -19,28 +21,37 @@ class SessionInterceptor constructor(
         private const val VALUE_TOKEN = "Bearer %s"
         private const val X_DM_API_REFRESH = "X-Dm-Api-Refresh"
         private const val USER_AGENT = "User-Agent"
+        private const val X_SYSTEM_INFO = "X-System-Info"
+        private const val ACCEPT_LANGUAGE = "Accept-Language"
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val token = preferenceStorage.authToken
 
         val originalRequest = chain.request()
-        val tokenRequest = originalRequest.newBuilder()
+        val customRequest = originalRequest.newBuilder()
 
         if (!token.isNullOrBlank()) {
-            tokenRequest.addHeader(KEY_TOKEN, String.format(VALUE_TOKEN, token))
+            customRequest.addHeader(KEY_TOKEN, String.format(VALUE_TOKEN, token))
         }
 
         if (BuildConfig.USER_AGENT.isNotBlank()) {
-            tokenRequest.addHeader(USER_AGENT, BuildConfig.USER_AGENT)
+            customRequest.addHeader(USER_AGENT, BuildConfig.USER_AGENT)
         }
 
         if (preferenceStorage.xDmApiRefresh || DataModule.xDmApiRefresh) {
-            tokenRequest.addHeader(X_DM_API_REFRESH, "")
+            customRequest.addHeader(X_DM_API_REFRESH, "")
             preferenceStorage.xDmApiRefresh = false
             DataModule.xDmApiRefresh = false
         }
 
-        return chain.proceed(tokenRequest.build())
+        // System Information header
+        val xSystemInfo = "Android,${Build.VERSION.RELEASE},${Build.MANUFACTURER},${Build.MODEL}"
+        customRequest.addHeader(X_SYSTEM_INFO, xSystemInfo)
+
+        // Language header
+        customRequest.addHeader(ACCEPT_LANGUAGE, Locale.getDefault().language)
+
+        return chain.proceed(customRequest.build())
     }
 }
